f4011e258b95ef128ceede95c2b6a6b561f91c0f
160197415 <abbasgure@gmail.com>
2023-05-02 23:42:33 +0100

    Fixed the sticky css feature for the navbar

--- header.php
+++ header.php
@@ -4,33 +4,35 @@ session_start();
 
 <!DOCTYPE html>
 <html>
+
 <head>
-<link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="styles.css">
+
 </head>
+
 <body>
 
-<nav>
-    <div class="wrapper">
-        <a href="index.php"></a>
-<ul>
-  <li><a class="active" href="index.php">Home</a></li>
-  <?php
 
-  if (isset($_SESSION["userName"])){
-    echo "<li><a href='profile.php'>Profile Page</a></li>";
-    echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";
-  }
+  <nav>
 
-  else {
-    echo "<li><a href='signup.php'>Sign Up</a></li>";
-    echo "<li><a href='login.php'>Log In</a></li>";
 
-  }
-?>
-</ul>
-</nav>
-</div>
+    <ul>
+      <li><a class="active" href="index.php">Home</a></li>
+      <?php
+
+      if (isset($_SESSION["userName"])) {
+        echo "<li><a href='profile.php'>Profile Page</a></li>";
+        echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";
+      } else {
+        echo "<li><a href='signup.php'>Sign Up</a></li>";
+        echo "<li><a href='login.php'>Log In</a></li>";
+      }
+      ?>
+    </ul>
+  </nav>
 
 
 
-<div class="wrapper">
+
+
+  <div class="wrapper">
--- login.php
+++ login.php
@@ -3,34 +3,34 @@ include_once 'header.php';
 ?>
 
 <section class="login-form center">
-    
-    <h2>Log In</h2>
-    <form action="includes/login.inc.php" method="post">
 
-        <input type="text" name="userName" placeholder="Username">
+  <h2>Log In</h2>
+  <form action="includes/login.inc.php" method="post">
 
-        <input type="password" name="password" placeholder="Password">
+    <input type="text" name="userName" placeholder="Username">
 
-        <button type="submit" name="submit">Log In</button>
+    <input type="password" name="password" placeholder="Password">
 
-    </form>
-    
+    <button type="submit" name="submit">Log In</button>
 
-    <?php
-      if (isset($_GET["error"])) {
-        if ($_GET["error"] == "emptyinput") {
-             echo "<p>Fill out the required fields please!</p>";   
-        }
-      }      
+  </form>
 
-      if (isset($_GET["error"])) {
-        if ($_GET["error"] == "wronglogin") {
-             echo "<p>Incorrect Login Information!</p>";   
-        }
-      }      
-        
-    
-    ?>
+
+  <?php
+  if (isset($_GET["error"])) {
+    if ($_GET["error"] == "emptyinput") {
+      echo "<p>Fill out the required fields please!</p>";
+    }
+  }
+
+  if (isset($_GET["error"])) {
+    if ($_GET["error"] == "wronglogin") {
+      echo "<p>Incorrect Login Information!</p>";
+    }
+  }
+
+
+  ?>
 </section>
 
 <?php

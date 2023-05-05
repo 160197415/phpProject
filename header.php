dfeb641d36f1773493bf32922ed3ce336954ead6
160197415 <abbasgure@gmail.com>
2023-05-05 01:03:55 +0100

    About and projects.php is empty, navbar looking functional for now.
    Co-authored-by: cabzmo <cabzmo@users.noreply.github.com>

--- header.php
+++ header.php
@@ -7,25 +7,29 @@ session_start();
 
 <head>
   <link rel="stylesheet" href="styles.css">
+  <!-- <link rel="stylesheet" href="newccsfile.css"> -->
 
 </head>
 
 <body>
 
 
-  <nav class="grid-container">
-  <div> <img src="imgs/Original.png" alt="logo" class="logo"/></div>
+  <nav class="grid-container navbar-collapse navbar-expand">
+
+  <a href="index.php"><div> <img src="imgs/Original.png" alt="logo" class="logo"/></div></a>
   <div></div>
   <div>
 
     <ul>
-      <li><a class="active" href="index.php">Home</a></li>
+      <li ><a class="active" href="about.php">About Us</a></li>
       <?php
 
       if (isset($_SESSION["userName"])) {
-        echo "<li><a href='profile.php'>Profile Page</a></li>";
+        echo "<li><a href='news.php'>News</a></li>";
+        echo "<li><a href='projects.php'>Projects and Services</a></li>";
         echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";
       } else {
+        echo "<li><a href='projects.php'>Projects and Services</a></li>";
         echo "<li><a href='signup.php'>Sign Up</a></li>";
         echo "<li><a href='login.php'>Log In</a></li>";
       }

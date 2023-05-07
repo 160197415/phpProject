4ac301f8727b080f4fe79d2c15abd6144d5336b7
160197415 <abbasgure@gmail.com>
2023-05-07 01:02:04 +0100

    Navbar positioning of li fixed, colourations fixed. cab sitting with me on this one
    Co-authored-by: cabzmo <cabzmo@users.noreply.github.com>

--- header.php
+++ header.php
@@ -10,32 +10,30 @@ session_start();
   <!-- <link rel="stylesheet" href="newccsfile.css"> -->
 
 </head>
-
 <body>
+  <nav class="grid-container">
 
+    <div class="grid-item">
+      <a href="index.php"> <img src="imgs/Original.png" alt="logo" class="logo"/></a>
+    </div>
+    <div class="grid-item"></div>
+    <div class="grid-item">
+      <ul>
+        <li ><a class="active" href="about.php">About Us</a></li>
+        <?php
 
-  <nav class="grid-container navbar-collapse navbar-expand">
-
-  <a href="index.php"><div> <img src="imgs/Original.png" alt="logo" class="logo"/></div></a>
-  <div></div>
-  <div>
-
-    <ul>
-      <li ><a class="active" href="about.php">About Us</a></li>
-      <?php
-
-      if (isset($_SESSION["userName"])) {
-        echo "<li><a href='news.php'>News</a></li>";
-        echo "<li><a href='projects.php'>Projects and Services</a></li>";
-        echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";
-      } else {
-        echo "<li><a href='projects.php'>Projects and Services</a></li>";
-        echo "<li><a href='signup.php'>Sign Up</a></li>";
-        echo "<li><a href='login.php'>Log In</a></li>";
-      }
-      ?>
-    </ul>
-  </div>
+        if (isset($_SESSION["userName"])) {
+          echo "<li><a href='news.php'>News</a></li>";
+          echo "<li><a href='projects.php'>Projects and Services</a></li>";
+          echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";
+        } else {
+          echo "<li><a href='projects.php'>Projects and Services</a></li>";
+          echo "<li><a href='signup.php'>Sign Up</a></li>";
+          echo "<li><a href='login.php'>Log In</a></li>";
+        }
+        ?>
+      </ul>
+    </div>
   </nav>
 
 
--- index.php
+++ index.php
@@ -25,14 +25,16 @@ if (isset($_SESSION["userName"])) {
 
 <section class="section section-light">
 
+<a href="#section-1">
 <h2>section 1</h2>
 <p>
   Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam tempore nihil nemo consequatur est laudantium labore iure cupiditate deserunt sunt, dolorem, ab dicta excepturi provident error non saepe? Libero nisi illum rerum rem nulla quas est tenetur quo voluptas dolorum voluptatem ipsum ipsam non natus molestias officia sed magni vero ut asperiores quae, quos perspiciatis accusamus minus! Repellendus minima tenetur minus? Debitis, expedita error ullam ipsum ipsam dolorum, deserunt consequatur exercitationem, nobis cum veniam. Quisquam sunt deserunt earum sit corporis quasi est eligendi delectus unde ipsum rerum ipsam, architecto impedit ab voluptates a, magnam atque. Autem dolores deleniti debitis corporis?
 </p>
+</a>
 
 </section>
 
-<div class="pimg2">
+<div class="pimg2" id="pimg21">
 <div class="ptext">
   <span class="border trans">
     Image two

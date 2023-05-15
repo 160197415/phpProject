46f2159cb15f5313216b3f4048c9b788d7189e9e
160197415 <abbasgure@gmail.com>
2023-05-16 00:43:24 +0100

    Fixed Navbar content for small and large screen hopefully

--- header.php
+++ header.php
@@ -20,19 +20,19 @@ session_start();
     <div class="grid-item">
       <a href="index.php"><img src="imgs/Original.png" alt="logo" class="logo" /></a>
     </div>
-    <div class="grid-item"></div>
+    <!-- <div class="grid-item"></div> -->
     <div class="grid-item-nav" id="myTopnav">
     
       <ul>
-      <li><a class="icon"><i class="fa fa-bars"></i></a></li>
-        <li><a class="active" href="about.php">About Us</a></li>     
+      <li><a class="icon" onclick="myFunction()"><i class="fa fa-bars"></i></a></li>
+        <li><a href="about.php">About Us</a></li>     
         <?php
         if (isset($_SESSION["userName"])) {
           echo "<li><a href='news.php'>News</a></li>";
-          echo "<li><a href='projects.php'>Projects and Services</a></li>";
+          echo "<li><a href='projects.php'>Projects & Services</a></li>";
           echo "<li><a href='includes/logout.inc.php'>Log Out</a></li>";  
         } else {
-          echo "<li><a href='projects.php'>Projects and Services</a></li>";
+          echo "<li><a href='projects.php'>Projects & Services</a></li>";
           echo "<li><a href='signup.php'>Sign Up</a></li>";
           echo "<li><a href='login.php'>Log In</a></li>";          
         }
